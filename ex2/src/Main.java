7d2c3986f7f21643d007d965534e8fc2cfb96f5e
kyle <kylenguyen60@hotmail.com>
2024-09-23 12:04:43 +0200

    test

---
+++ ex2/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+    ListerMot listerMot = new ListerMot("test.txt");
+
+    CommencePartT commencePartT = new CommencePartT();
+    DeLongueur deLongueur = new DeLongueur();
+    deLongueur.setLongueur(3);
+    Palindrome palindrome = new Palindrome();
+
+    listerMot.imprimerSi(palindrome);
+
+  }
+
+}
