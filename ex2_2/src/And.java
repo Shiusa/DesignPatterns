efc4bcc49369465f92ee40bf0c0c19864234be0b
kyle <kylenguyen60@hotmail.com>
2024-09-23 12:24:54 +0200

    add And

---
+++ ex2_2/src/And.java
@@ -0,0 +1,14 @@
+public class And implements Strategy{
+  Strategy strategy1;
+  Strategy strategy2;
+
+  public And(Strategy strategy1, Strategy strategy2) {
+    this.strategy1 = strategy1;
+    this.strategy2 = strategy2;
+  }
+
+  @Override
+  public boolean estValide(String mot) {
+    return strategy1.estValide(mot) && strategy2.estValide(mot);
+  }
+}
