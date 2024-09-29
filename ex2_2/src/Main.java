d9b444a1ad838b90eb7f512427d827f63ea7733c
Kyle_Home <trong.nguyen@student.vinci.com>
2024-09-29 23:32:16 +0200

    add: Not

--- ex2_2/src/Main.java
+++ ex2_2/src/Main.java
@@ -7,11 +7,11 @@ public class Main {
     ListerMot listerMot = new ListerMot("test.txt");
 
     CommencePartT commencePartT = new CommencePartT();
-    DeLongueur deLongueur = new DeLongueur(3);
+    DeLongueur deLongueur = new DeLongueur(1);
     //deLongueur.setLongueur(3);
     Palindrome palindrome = new Palindrome();
 
-    listerMot.imprimerSi(new And(deLongueur,commencePartT));
+    listerMot.imprimerSi(new Not(deLongueur,commencePartT));
 
   }
 
